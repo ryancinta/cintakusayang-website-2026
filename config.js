33c88977fc8e9e15ff28ca356350d088ea76ea85
ryancinta <ryandsa2004@gmail.com>
2026-02-15 03:57:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Kamu sayangg ryann? 💝",
+    pageTitle: "MY BUBUUU TESTTTT💛💛💛",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
